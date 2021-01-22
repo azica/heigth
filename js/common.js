bc20a11e0119cb787bbe90dc05a7ba2a7aae005b
Aizada <azicakcl@mail.ru>
2021-01-22 23:11:47 +0600

    add plugin

--- js/common.js
+++ js/common.js
@@ -1,12 +1,51 @@
+var matchHeight = function () {
+	
+	function init() {
+		eventListeners();
+		matchHeight();
+	}
+	
+	function eventListeners(){
+		$(window).on('resize', function() {
+			matchHeight();
+		});
+	}
+	
+	function matchHeight(){
+		var groupName = $('h5');
+		var groupHeights = [];
+		
+		groupName.css('min-height', 'auto');
+		
+		groupName.each(function() {
+			groupHeights.push($(this).outerHeight());
+		});
+		
+		var maxHeight = Math.max.apply(null, groupHeights);
+		groupName.css('min-height', maxHeight);
+	};
+	
+	return {
+		init: init
+	};
+	
+} ();
 
-$(document).ready(function(){
-  var largest = 0;
-  //loop through all title elements
-  var height = 0;
-  $('h5').each(function(){
-     var currentHeight= $(this).height();
-      height = currentHeight > height ? currentHeight : height; 
-  });
-  $('h5').css('height', height); 
-
+$(document).ready(function() {
+	matchHeight.init();
 });
+
+
+
+
+// $(document).ready(function(){
+//   var largest = 0;
+//   //loop through all title elements
+//   var height = 0;
+//   $('h5').each(function(){
+//      var currentHeight= $(this).height();
+//       height = currentHeight > height ? currentHeight : height; 
+//   });
+//   $('h5').css('height', height); 
+
+// });
